443f4bdeb42bdf89c4cb7529f24b5d1591efb309
Dmitry Kostyaev <d.kostyaev@babo.com>
2016-03-02 12:41:41 +0300

    Add logging

--- start.py
+++ start.py
@@ -2,10 +2,13 @@ import signal
 import sys
 from apscheduler.schedulers.blocking import BlockingScheduler
 from actors import IndexManager
+from logging import config as _config
+import logging
 
 scheduler = BlockingScheduler()
 index_manager = IndexManager.start().proxy()
-
+_config.fileConfig('logging.conf')
+logger = logging.getLogger("AnnSearcher")
 
 @scheduler.scheduled_job('interval', minutes=30)
 def compaction_job():
@@ -18,14 +21,14 @@ def build_new_indices():
 
 
 def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
+        logger.info('You pressed Ctrl+C!')
         sys.exit(0)
 
 
 
 if __name__ == '__main__':
         signal.signal(signal.SIGINT, signal_handler)
-        print('Application started, press Ctrl+C to save state and exit')
+        logger.info('Application started, press Ctrl+C to save state and exit')
         scheduler.start()
         signal.pause()
 
